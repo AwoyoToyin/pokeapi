213291752ba7c89198bda2b07d828eb4e681483c
Oluwatoyin Awoyo <oluwatoyin.a@cars45.com>
2019-10-20 20:39:52 +0100

    feat: shaved off the trailing'/api/v1' from the routes

--- service/src/controllers/poke.controller.spec.ts
+++ service/src/controllers/poke.controller.spec.ts
@@ -14,7 +14,7 @@ describe('PokeController', () => {
 
     test('Should return a 500 Internal Server Error when called without supplying one|more names', async () => {
         try {
-            const response = await request(app.getApp()).get('/api/v1/strongest');
+            const response = await request(app.getApp()).get('/strongest');
             expect(response.status).toEqual(500);
             expect(response.body).toHaveProperty('message');
         } catch (error) { }
@@ -22,7 +22,7 @@ describe('PokeController', () => {
 
     test('Should return the most powerful pokemon from supplied pokemon names', async () => {
         try {
-            const response = await request(app.getApp()).get('/api/v1/strongest?names=butterfree,wormadam-plant');
+            const response = await request(app.getApp()).get('/strongest?names=butterfree,wormadam-plant');
             expect(response.status).toEqual(200);
             expect(response.body).toHaveProperty('code');
         } catch (error) { }
